e0ed64f6aea5f5c69b8ba54d2e3b0bd182f7abd0
NickB521 <nicolasblackson@gmail.com>
2022-07-19 13:23:48 +0000

    first commit

---
+++ forgot-password/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Forgot from './components/Forgot';
+
+function App() {
+  return (
+    <div className="App">
+      <Forgot></Forgot>
+    </div>
+  );
+}
+
+export default App;
